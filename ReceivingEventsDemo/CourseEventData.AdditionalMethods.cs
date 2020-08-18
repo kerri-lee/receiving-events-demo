8e67a2cec747cf005e9a97ad6133c1ef6c9245e9
Kerri Lee <52222716+kerri-lee@users.noreply.github.com>
2020-08-17 18:10:57 -0700

    Add files via upload

---
+++ ReceivingEventsDemo/CourseEventData.AdditionalMethods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReceivingEventsDemo
+{
+    public partial class CourseEventData
+    {
+        public override string ToString()
+        {
+            // formatting for console
+            ChangeConsoleColor(Department);
+            return "Course: " + Department + " " + CourseNumber + "\tStart Time: " + StartTime.ToString("t");
+        }
+
+        private static void ChangeConsoleColor(string department)
+        {
+            Console.ForegroundColor = department switch
+            {
+                "MATH" => ConsoleColor.Green,
+                "PHYS" => ConsoleColor.Blue,
+                "CSE" => ConsoleColor.Yellow,
+                "BIOL" => ConsoleColor.Red,
+                "ENGL" => ConsoleColor.Cyan,
+                _ => ConsoleColor.White,
+            };
+        }
+    }
+}
---
+++ ReceivingEventsDemo/CourseEventData.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReceivingEventsDemo
+{
+    public partial class CourseEventData
+    {
+        public string Department { get; set; }
+
+        public int CourseNumber { get; set; }
+
+        public DateTimeOffset StartTime { get; set; }
+    }
+}
